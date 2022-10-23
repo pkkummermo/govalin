4d05f490f40087bd301a53c92534291e9eb35043
Per Kristian Kummermo <pkkummermo@lab5e.com>
2022-10-23 18:26:24 +0200

    feat(govalinHTTP): add missing http methods

--- internal/govalintesting/testing.go
+++ internal/govalintesting/testing.go
@@ -2,6 +2,7 @@ package govalintesting
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"time"
 
@@ -24,7 +25,7 @@ type GovalinHTTP struct {
 	Host string
 }
 
-func (govalinHttp *GovalinHTTP) Get(path string, params ...interface{}) string {
+func (govalinHttp *GovalinHTTP) Get(path string, params ...any) string {
 	url := govalinHttp.Host + path
 	response, err := govalinHttp.http.Get(url, params...)
 	if err != nil {
@@ -39,7 +40,7 @@ func (govalinHttp *GovalinHTTP) Get(path string, params ...interface{}) string {
 	return data
 }
 
-func (govalinHttp *GovalinHTTP) GetResponse(path string, params ...interface{}) *httpclient.Response {
+func (govalinHttp *GovalinHTTP) GetResponse(path string, params ...any) *httpclient.Response {
 	url := govalinHttp.Host + path
 	response, err := govalinHttp.http.Get(url, params...)
 	if err != nil {
@@ -64,9 +65,84 @@ func (govalinHttp *GovalinHTTP) Post(path string, postData any) string {
 	return data
 }
 
-func (govalinHttp *GovalinHTTP) PostResponse(path string, postData interface{}) *httpclient.Response {
+func (govalinHttp *GovalinHTTP) PostResponse(path string, postData any) *httpclient.Response {
 	url := govalinHttp.Host + path
 	response, err := govalinHttp.http.Post(url, postData)
+	if err != nil {
+		log.Fatalf("HTTP: Failed to POST %s. %v", url, err)
+	}
+
+	return response
+}
+
+func (govalinHttp *GovalinHTTP) Put(path string, putData io.Reader) string {
+	url := govalinHttp.Host + path
+	response, err := govalinHttp.http.Put(url, putData)
+	if err != nil {
+		log.Fatalf("HTTP: Failed to PUT %s. %v", url, err)
+	}
+
+	data, err := response.ToString()
+	if err != nil {
+		log.Fatalf("HTTP: Failed decode PUT response as string for %s. %v", url, err)
+	}
+
+	return data
+}
+
+func (govalinHttp *GovalinHTTP) PutResponse(path string, putData io.Reader) *httpclient.Response {
+	url := govalinHttp.Host + path
+	response, err := govalinHttp.http.Put(url, putData)
+	if err != nil {
+		log.Fatalf("HTTP: Failed to PUT %s. %v", url, err)
+	}
+
+	return response
+}
+
+func (govalinHttp *GovalinHTTP) Patch(path string, patchData map[string]string) string {
+	url := govalinHttp.Host + path
+	response, err := govalinHttp.http.Patch(url, patchData)
+	if err != nil {
+		log.Fatalf("HTTP: Failed to PATCH %s. %v", url, err)
+	}
+
+	data, err := response.ToString()
+	if err != nil {
+		log.Fatalf("HTTP: Failed decode PATCH response as string for %s. %v", url, err)
+	}
+
+	return data
+}
+
+func (govalinHttp *GovalinHTTP) PatchResponse(path string, patchData map[string]string) *httpclient.Response {
+	url := govalinHttp.Host + path
+	response, err := govalinHttp.http.Patch(url, patchData)
+	if err != nil {
+		log.Fatalf("HTTP: Failed to PATCH %s. %v", url, err)
+	}
+
+	return response
+}
+
+func (govalinHttp *GovalinHTTP) Delete(path string, deleteData ...any) string {
+	url := govalinHttp.Host + path
+	response, err := govalinHttp.http.Delete(url, deleteData...)
+	if err != nil {
+		log.Fatalf("HTTP: Failed to DELETE %s. %v", url, err)
+	}
+
+	data, err := response.ToString()
+	if err != nil {
+		log.Fatalf("HTTP: Failed decode DELETE response as string for %s. %v", url, err)
+	}
+
+	return data
+}
+
+func (govalinHttp *GovalinHTTP) DeleteResponse(path string, deleteData ...any) *httpclient.Response {
+	url := govalinHttp.Host + path
+	response, err := govalinHttp.http.Delete(url, deleteData...)
 	if err != nil {
 		log.Fatalf("HTTP: Failed to GET %s. %v", url, err)
 	}
