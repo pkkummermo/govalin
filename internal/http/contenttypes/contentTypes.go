b370ea8faf91f4b4b718fa188f63e92b9cc608b5
Per Kristian Kummermo <per.kristian@kummermo.no>
2023-10-10 08:54:08 +0200

    feat(contentTypes): add missing contentTypes and add doc

--- internal/http/contenttypes/contentTypes.go
+++ internal/http/contenttypes/contentTypes.go
@@ -1,6 +1,14 @@
 package contenttypes
 
 const (
-	MultipartFormData         = "multipart/form-data"
+	// ApplicationFormURLEncoded is the content type for form data.
 	ApplicationFormURLEncoded = "application/x-www-form-urlencoded"
+	// ApplicationJSON is the content type for JSON data.
+	ApplicationJSON = "application/json"
+	// MultiPartFormData is the content type for multipart form data.
+	MultipartFormData = "multipart/form-data"
+	// TextHTML is the content type for HTML data.
+	TextHTML = "text/html"
+	// TextPlain is the content type for plain text data.
+	TextPlain = "text/plain"
 )
