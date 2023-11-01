e0c7176331566a3603509add250e96789c74b792
Per Kristian Kummermo <per.kristian@kummermo.no>
2023-11-01 09:24:18 +0100

    feat(inmemory-session): add implementation of in-memory session store

---
+++ internal/session/in-memory.go
@@ -0,0 +1,174 @@
+// Package session provides an interface for managing user sessions.
+// This file contains an implementation of the Store interface using an in-memory store.
+package session
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"sync"
+	"time"
+)
+
+const pruneInterval = 1 * time.Minute
+
+// inMemoryStore is an implementation of the Store interface using an in-memory store.
+type inMemoryStore struct {
+	mutex *sync.Mutex
+	store map[string]Session
+}
+
+// NewInMemoryStore returns a new instance of inMemoryStore.
+func NewInMemoryStore() Store {
+	initiatedStore := inMemoryStore{
+		mutex: &sync.Mutex{},
+		store: make(map[string]Session),
+	}
+
+	go func() {
+		for {
+			time.Sleep(pruneInterval)
+			err := initiatedStore.sessionPrune()
+			if err != nil {
+				slog.Error(fmt.Sprintf("Failed to prune sessions: %v", err))
+			}
+		}
+	}()
+
+	return &initiatedStore
+}
+
+func (s *inMemoryStore) sessionPrune() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	for sessionID, session := range s.store {
+		if session.Expires < time.Now().UnixNano() {
+			slog.Debug(fmt.Sprintf("Pruning session: %s", sessionID))
+			deleteErr := s.RemoveSession(sessionID)
+			if deleteErr != nil {
+				return deleteErr
+			}
+		}
+	}
+	return nil
+}
+
+// CreateSession creates a new session with the given expiration time and returns its ID.
+func (s *inMemoryStore) CreateSession(expires int64) (string, error) {
+	sessionID, err := createNewSessionID(s)
+
+	if err != nil {
+		return "", err
+	}
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.store[sessionID] = Session{
+		ID:      sessionID,
+		Expires: expires,
+		Data:    Data{},
+	}
+
+	return sessionID, nil
+}
+
+// GetSession returns the session with the given ID if it exists and has not expired.
+func (s *inMemoryStore) GetSession(sessionID string, expireTime int64) (Session, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if session, ok := s.store[sessionID]; ok {
+		if session.Expires > expireTime {
+			return session, nil
+		}
+	}
+
+	return Session{}, errors.New("not found")
+}
+
+// RemoveSession removes the session with the given ID if it exists.
+func (s *inMemoryStore) RemoveSession(sessionID string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if _, ok := s.store[sessionID]; !ok {
+		return errors.New("not found")
+	}
+
+	delete(s.store, sessionID)
+	return nil
+}
+
+// GetSessions returns all sessions that have not expired.
+func (s *inMemoryStore) GetSessions(time int64) ([]Session, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	var sessions = make([]Session, 0)
+
+	for _, session := range s.store {
+		if session.Expires > time {
+			sessions = append(sessions, session)
+		}
+	}
+
+	return sessions, nil
+}
+
+// RefreshSession extends the expiration time of the session with the given ID.
+func (s *inMemoryStore) RefreshSession(sessionID string, checkInterval int64) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if session, ok := s.store[sessionID]; ok {
+		session.Expires += checkInterval
+		s.store[sessionID] = session
+
+		return nil
+	}
+
+	return errors.New("not found")
+}
+
+// SetSessionData sets the data associated with the session with the given ID.
+func (s *inMemoryStore) SetSessionData(sessionID string, data Data) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if session, ok := s.store[sessionID]; ok {
+		session.Data = data
+		s.store[sessionID] = session
+		return nil
+	}
+
+	return errors.New("not found")
+}
+
+// GetSessionData returns the data associated with the session with the given ID.
+func (s *inMemoryStore) GetSessionData(sessionID string) (Data, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if session, ok := s.store[sessionID]; ok {
+		return session.Data, nil
+	}
+
+	return Data{}, errors.New("not found")
+}
+
+// RemoveSessionData removes the data associated with the session with the given ID.
+func (s *inMemoryStore) RemoveSessionData(sessionID string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if session, ok := s.store[sessionID]; ok {
+		session.Data = Data{}
+		s.store[sessionID] = session
+
+		return nil
+	}
+
+	return errors.New("not found")
+}
---
+++ internal/session/session-store.go
@@ -0,0 +1,87 @@
+package session
+
+import (
+	"crypto/rand"
+	"database/sql/driver"
+	"encoding/base64"
+	"errors"
+	"io"
+
+	"log/slog"
+
+	"github.com/pkkummermo/govalin/internal/util"
+)
+
+// Data is the stored session data.
+type Data map[string]interface{}
+
+// Scan implements the sql.Scanner interface.
+func (p *Data) Scan(src interface{}) error {
+	return util.ScanJSON(p, src)
+}
+
+// Value implements the driver.Valuer interface.
+func (p *Data) Value() (driver.Value, error) {
+	return util.ValueJSON(p)
+}
+
+// Session is the stored session data.
+type Session struct {
+	ID      string
+	Expires int64
+	Data    Data
+}
+
+// Store is the interface for the session back-end store.
+type Store interface {
+	// CreateSession creates a new session in the store. The expires parameter is the expire
+	// time in nanonseconds for the session. Returns the session ID.
+	CreateSession(expires int64) (string, error)
+
+	// GetSession returns the session from the session store based
+	// on given ID and where expire is higher than provided nanoseconds.
+	GetSession(sessionID string, ingnoreOlderNs int64) (Session, error)
+
+	// RemoveSession removes the session from the store.
+	RemoveSession(sessionID string) error
+
+	// GetSessions returns sessions with expire time is less than given time in nanoseconds.
+	GetSessions(time int64) ([]Session, error)
+
+	// RefreshSession refreshes a session expire time by adding the given nanoseconds.
+	RefreshSession(sessionID string, expireAddNs int64) error
+
+	// SetSessionData sets the session data in the store.
+	SetSessionData(sessionID string, data Data) error
+
+	// GetSessionData returns the session data from the store.
+	GetSessionData(sessionID string) (Data, error)
+
+	// RemoveSessionData removes the session data from the store.
+	RemoveSessionData(sessionID string) error
+}
+
+// createNewSessionID generates a random session id. Also
+// check for collisions so we don't accidentally assign an existing session id.
+func createNewSessionID(s Store) (string, error) {
+	numCollisionsBeforeWeGiveUp := 5
+	for i := 0; i < numCollisionsBeforeWeGiveUp; i++ {
+		randSize := 64
+		randomBytes := make([]byte, randSize)
+		if _, err := io.ReadFull(rand.Reader, randomBytes); err != nil {
+			slog.Error("Unable to generate random data for session ID")
+			return "", err
+		}
+
+		encoded := base64.URLEncoding.EncodeToString(randomBytes)
+		if _, err := s.GetSession(encoded, 0); err != nil {
+			// Return the session id if we don't find it in the store.
+			//nolint: nilerr // If we don't find the session we consider that a success.
+			return encoded, nil
+		}
+	}
+
+	// We end up here if we have given up creating a session id.
+	slog.Error("Unable to create a unique session ID")
+	return "", errors.New("unable to create a unique session ID")
+}
